1f4fa100d022bce13b8feaebfee2f41abb33dbe9
SuryaJ9 <95493040+SuryaJ9@users.noreply.github.com>
2023-11-29 22:08:12 +0530

    Update module-info.java

--- module-info.java
+++ module-info.java
@@ -5,4 +5,33 @@
  * 
  */
 module Practise {
+   public String longestPrefix(String[] str) {
+        if (str == null || str.length == 0) {
+            return "";
+        }
+
+        Arrays.sort(str);
+        StringBuilder str1 = new StringBuilder();
+
+        char[] first = str[0].toCharArray();
+        char[] last = str[str.length - 1].toCharArray();
+
+        for (int i = 0; i < first.length && i < last.length; i++) {
+            if (first[i] == last[i]) {
+                str1.append(first[i]);
+            } else {
+                break; // Stop when a mismatch is found
+            }
+        }
+
+        return str1.toString();
+    }
+
+    public static void main(String[] args) {
+        Practise obj = new Practise();
+        String[] str = {"jai", "jaya", "jay"};
+        System.out.println("Longest prefix: " + obj.longestPrefix(str));
+    }
+}
+
 }
